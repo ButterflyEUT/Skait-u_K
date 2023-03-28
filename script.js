b9f74674e22576c70e6bcfdac0a8ea26fa1fba18
Elīza Unigunda Tormane <119410780+ButterflyEUT@users.noreply.github.com>
2023-03-28 12:38:28 +0300

    Jauni elementi, kuri ir apgūti
    d

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const input1 = document.querySelector('.js-teksts')
 const plusButton = document.querySelector('.js-plus')
 
+const add = (skaitlis1, skaitlis2) => {
+    return skaitlis1 + skaitlis2
+}
 
-console.log(input1)
-console.log(plusButton)
+const subtract = (skaitlis1, skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const multipy = (skaitlis1, skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const divide = (skaitlis1, skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click', () => {
+    console.log("Mani nospieda!")
+} )
