d517808911ce727826fd3994e7bec6261c9f7c9b
Elīza Unigunda Tormane <119410780+ButterflyEUT@users.noreply.github.com>
2023-04-04 12:32:21 +0300

    pirmā strādājošā poga

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const input1 = document.querySelector('.js-teksts')
+const input1 = document.querySelector('.js-teksts1')
+const input2 = document.querySelector('.js-teksts2')
 const plusButton = document.querySelector('.js-plus')
+const minusButton = document.querySelector ('.js-minus')
+const multiplyButton = document.querySelector ('.js-multiply')
+const divideButton = document.querySelector ('.js-divide')
+const result = document.querySelector ('.js-result')
 
-const add = (skaitlis1, skaitlis2) => {
-    return skaitlis1 + skaitlis2
+const Add = (skaitlis1, skaitlis2) => {
+    return Number(skaitlis1)+ Number(skaitlis2)
 }
 
 const subtract = (skaitlis1, skaitlis2) => {
@@ -18,5 +23,5 @@ const divide = (skaitlis1, skaitlis2) => {
 }
 
 plusButton.addEventListener('click', () => {
-    console.log("Mani nospieda!")
+   result.innerHTML = Add (input1.value, input2.value)
 } )
